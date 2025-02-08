a401bc5f5b83d1aea116121ae02a6d56e0284d4b
saheer <saheerabcd3@gmail.com>
2025-02-08 19:22:12 +0530

    feat: added Dockerfile

--- contestant/main.py
+++ contestant/main.py
@@ -9,4 +9,4 @@ app.include_router(router, prefix="/api/v1")
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8001)
+    uvicorn.run(app, host="0.0.0.0", port=8001, reload=True)
