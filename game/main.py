1713fc378dbd9821c07351f48d0c894767b1b2a5
saheer <saheerabcd3@gmail.com>
2025-02-11 17:54:03 +0530

    feat: added router in game

--- game/main.py
+++ game/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
 
+from app.api.v1.rest.game import game_router
+from app.api.v1.rest.game_participant import game_participant_router
+
 
 app = FastAPI(title="Game Service")
+
+
+app.include_router(game_router)
+app.include_router(game_participant_router)
 
 
 if __name__ == "__main__":
